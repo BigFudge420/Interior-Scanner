836f191552b874703eb011d60c57d85b21274b21
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-16 02:46:54 +0530

    feat: set up react-router-dom and initialize app routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Scanner from './pages/Scanner.jsx'
+
 function App() {
+  
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/scanner" element={<Scanner/>} />
+      </Routes>
       <h2>Interior Scanner</h2>
-      <p>YET TO START DEVELOPMENT</p>
-    </>
+      <p>Navigate to '/login' for the Login page</p>
+      <p>Navigate to '/dashboard' for the Dashboard page</p>
+      <p>Navigate to '/scanner' for the Scanner page</p>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Scanner.tsx
@@ -0,0 +1,7 @@
+export default function Scanner() {
+    return (
+        <>
+            <h1>Scanner</h1>
+        </>
+    )
+}
