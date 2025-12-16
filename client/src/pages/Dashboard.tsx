f9d237d5b633756be7ea46da03f1707ed7fd95e3
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-16 22:19:49 +0530

    style: margin tweaks

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -12,7 +12,7 @@ export default function Dashboard() {
   return (
     <div className="relative">
       {scans && 
-      <>
+      <div className="m-3">
       <h2 className="text-4xl my-4 font-semibold">Your snaps,</h2>
       <div className="my-4 grid grid-cols-1 gap-4 items-start sm:grid-cols-2 lg:grid-cols-3">
         {scans.map((scan) => (
@@ -35,7 +35,7 @@ export default function Dashboard() {
                 <Camera />
             </Button>
         </ButtonGroup>
-      </>
+      </div>
       }
       {!scans && 
       <div className="flex justify-center items-center">
