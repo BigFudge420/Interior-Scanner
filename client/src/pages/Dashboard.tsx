1dcd08ee1ab8d89535f4310da43647319c0ec919
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-12-16 22:27:31 +0530

    style: add nav buttons to fallback page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -13,28 +13,20 @@ export default function Dashboard() {
     <div className="relative">
       {scans.length > 0 && 
       <div className="m-3">
-      <h2 className="text-4xl my-4 font-semibold">Your snaps,</h2>
-      <div className="my-4 grid grid-cols-1 gap-4 items-start sm:grid-cols-2 lg:grid-cols-3">
-        {scans.map((scan) => (
-          <Card key={scan.id} className="p-0 overflow-hidden">
-            <CardContent className="p-0">
-              <img
-                src={scan.imageURL}
-                alt="Room scan"
-                className="block w-full h-auto"
-              />
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-        <ButtonGroup className="fixed left-6/12 bottom-6 -translate-x-1/2 z-50">
-            <Button size={"icon"} variant={"outline"} className="h-14 w-14" onClick={() => nav('/')}>
-                <House />
-            </Button>
-            <Button size="icon" variant={"outline"} className="h-14 w-14" onClick={() => nav('/scanner')}>
-                <Camera />
-            </Button>
-        </ButtonGroup>
+        <h2 className="text-4xl my-4 font-semibold">Your snaps,</h2>
+        <div className="my-4 grid grid-cols-1 gap-4 items-start sm:grid-cols-2 lg:grid-cols-3">
+          {scans.map((scan) => (
+            <Card key={scan.id} className="p-0 overflow-hidden">
+              <CardContent className="p-0">
+                <img
+                  src={scan.imageURL}
+                  alt="Room scan"
+                  className="block w-full h-auto"
+                />
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
       }
       {scans.length === 0 && 
@@ -42,6 +34,14 @@ export default function Dashboard() {
         <h1 className="text-4xl font-semibold">Oops! Looks like you don't have any snaps saved.</h1>
       </div>
       }
+      <ButtonGroup className="fixed left-6/12 bottom-6 -translate-x-1/2 z-50">
+              <Button size={"icon"} variant={"outline"} className="h-14 w-14" onClick={() => nav('/')}>
+                  <House />
+              </Button>
+              <Button size="icon" variant={"outline"} className="h-14 w-14" onClick={() => nav('/scanner')}>
+                  <Camera />
+              </Button>
+          </ButtonGroup>
     </div>
   );
 }
